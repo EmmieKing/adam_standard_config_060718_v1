d1e1895a5a98e011a911c9ebacfb21ee7c26e85e
“EmmieKing” <Emmie.King@gmail.com>
2018-06-14 12:21:35 -0400

    syncing git repo

--- adam_standard_config_060718_v1/SEE_config_AdamToSTK_060718_v1.py
+++ adam_standard_config_060718_v1/SEE_config_AdamToSTK_060718_v1.py
@@ -1,7 +1,7 @@
 import sys, adam_config_060718_v1
 
 print(adam_config_060718_v1.setPaths.home)
-print(adam_config_060718_v1.setPaths.raw_config)
+#print(adam_config_060718_v1.setPaths.raw_config)
 print(adam_config_060718_v1.setPaths.adam_path)
 print(adam_config_060718_v1.setPaths.data_path)
 print(adam_config_060718_v1.setPaths.env_config_path)
